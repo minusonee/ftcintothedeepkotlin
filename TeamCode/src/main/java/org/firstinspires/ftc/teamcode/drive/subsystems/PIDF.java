03bb48d5ba86dd023edecc0ff8a7e234766568af
calin <swiorx@gmail.com>
2024-09-09 23:59:47 +0300

    pid test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/subsystems/PIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/subsystems/PIDF.java
@@ -5,7 +5,9 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @Config
@@ -18,20 +20,30 @@ public class PIDF extends OpMode {
 
     public static int target = 0;
 
-    private final double ticks_in_degree = 537.7 / 360;
+    private final double ticks_in_degree = 536 / 360;
 
-    private DcMotorEx motorCraneLeft;
+    private DcMotorEx motorCraneLeft,motorCraneRight;
 
     @Override
     public void init(){
         controller = new PIDController(p, i, d);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
+
+
         motorCraneLeft = hardwareMap.get(DcMotorEx.class, "motorCraneLeft");
+        motorCraneRight = hardwareMap.get(DcMotorEx.class, "motorCraneRight");
+
+        motorCraneLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorCraneLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorCraneLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorCraneRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorCraneRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorCraneRight.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void loop(){
-        controller.setPID(p,i,d);
+        controller.setPID(p, i, d);
         int armPos = motorCraneLeft.getCurrentPosition();
         double pid = controller.calculate(armPos, target);
         double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
@@ -39,9 +51,11 @@ public class PIDF extends OpMode {
         double power = pid + ff;
 
         motorCraneLeft.setPower(power);
+        motorCraneRight.setPower(power);
 
         telemetry.addData("pos ", armPos);
         telemetry.addData("target ", target);
+        telemetry.addData("Power ", power);
         telemetry.update();
     }
 }
